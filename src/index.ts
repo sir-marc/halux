6f359918a934654fa497dbab4622093149a6a6ba
laptop\marcb <marc.berli@windowslive.com>
2017-02-04 10:34:00 +0100

    refactor findSchemaWithLocation util to curry

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { SchemaI, SchemaWithLocationI } from './interfaces/SchemaInterface';
 import { HaluxActionI } from './interfaces/HaluxInterface';
-import { findSchemaWithLocation } from './utils/schemaUtils';
+import { findSchemaWithLocation, forEachReference } from './utils/schemaUtils';
 
 interface ActionI extends HaluxActionI {
 	type: string,
@@ -14,11 +14,16 @@ const createHalux = (schemasWithLocation: SchemaWithLocationI[]) => {
 		if (action.metadata.halux.go) {
 			const { metadata, payload } = action;
 			const { halux } = metadata
+
+			const schemaFinder = findSchemaWithLocation(schemasWithLocation)
 		
-			const { schema, location } = findSchemaWithLocation(schemasWithLocation, halux.schema)
+			const { schema, location } = schemaFinder(halux.schema)
 
 			if (schema && schema.references) {
+				forEachReference(schema, (refSchema) => {
+					const { location: loc } = schemaFinder(refSchema);
 
+				})
 			} else {
 				console.warn(`your schema "${halux.schema.name}" could not be found, is undefined or has no references`)
 			}
--- src/utils/schemaUtils.ts
+++ src/utils/schemaUtils.ts
@@ -1,9 +1,10 @@
 import { SchemaWithLocationI, SchemaI } from '../interfaces/SchemaInterface';
 
 
-export const findSchemaWithLocation = (schemasWithLocation: SchemaWithLocationI[], schema: SchemaI) => 
+export const findSchemaWithLocation = (schemasWithLocation: SchemaWithLocationI[]) =>
+	(schema: SchemaI) =>
 		schemasWithLocation.filter(swl => swl.schema.name === schema.name)[0];
 
 export const forEachReference = (schema: SchemaI, callback: (schema: SchemaI) => any) => {
-		Object.keys(schema.references).forEach(key => callback(schema.references[key]))
-	}
+	Object.keys(schema.references).forEach(key => callback(schema.references[key]))
+}
