cf2fe104a47fdd122348e507a53e32d57d9189d3
laptop\marcb <marc.berli@windowslive.com>
2017-02-04 10:29:50 +0100

    fix missed imports

--- src/utils/schemaUtils.ts
+++ src/utils/schemaUtils.ts
@@ -1,4 +1,7 @@
-export const findSchemaWithLocation = (schemasWithLocation, schema: SchemaI) => 
+import { SchemaWithLocationI, SchemaI } from '../interfaces/SchemaInterface';
+
+
+export const findSchemaWithLocation = (schemasWithLocation: SchemaWithLocationI[], schema: SchemaI) => 
 		schemasWithLocation.filter(swl => swl.schema.name === schema.name)[0];
 
 export const forEachReference = (schema: SchemaI, callback: (schema: SchemaI) => any) => {
