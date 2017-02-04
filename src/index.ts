fc0b25bf056d3f2c5011b20de3b69f36a4df31fd
laptop\marcb <marc.berli@windowslive.com>
2017-02-04 01:58:09 +0100

    first step to create middleware

--- src/index.ts
+++ src/index.ts
@@ -27,3 +27,29 @@ interface SchemaWithLocation {
 	location: string,
 }
 
+const createHalux = (schemasWithLocation: SchemaWithLocation[]) => {
+	const findSchemaWithLocation = (schema: SchemaI) => 
+		schemasWithLocation.filter(swl => swl.schema.name === schema.name)[0];
+
+	const forEachReference = (schema: SchemaI, callback: (schema: SchemaI) => any) => {
+		Object.keys(schema.references).forEach(key => callback(schema.references[key]))
+	}
+
+	return (store: any) => (next: any) => (action: ActionI) => {
+		if (action.metadata.halux.go) {
+			const { metadata, payload } = action;
+			const { halux } = metadata
+		
+			const { schema, location } = findSchemaWithLocation(halux.schema)
+
+			if (schema && schema.references) {
+
+			} else {
+				console.warn(`your schema "${halux.schema.name}" could not be found, is undefined or has no references`)
+			}
+		}
+		// middleware is not needed
+		return next(action)
+	}
+
+} 
