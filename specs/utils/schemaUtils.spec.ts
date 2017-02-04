ef54589d249a4cc56ccf37b089b869c6c4c89fec
laptop\marcb <marc.berli@windowslive.com>
2017-02-04 11:12:00 +0100

    added test setup and test for util methods

---
+++ specs/utils/schemaUtils.spec.ts
@@ -0,0 +1,82 @@
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+import * as sinonChai from 'sinon-chai';
+import { SchemaWithLocationI, SchemaI } from '../../src/interfaces/SchemaInterface';
+import { findSchemaWithLocation, forEachReference } from '../../src/utils/schemaUtils';
+
+const schemas: SchemaI[] = [
+	{ name: 'schemaOne'},
+	{ name: 'schemaTwo'},
+	{ name: 'schemaThree'},
+	{
+		name: 'schemaWithReferences',
+		references: {
+			'subSchemaOne': { name: 'subSchemaOne' },
+			'subSchemaTwo': { name: 'subSchemaTwo' },
+			'subSchemaThree': { name: 'subSchemaThree' },
+		}
+	}
+]
+
+const schemasWithLocation: SchemaWithLocationI[] = [
+	{ schema: schemas[0], location: 'path.to.schema'},
+	{ schema: schemas[1], location: 'path.to.schema'},
+	{ schema: schemas[2], location: 'path.to.schema'},
+];
+
+chai.use(sinonChai);
+
+describe('the schema utils', () => {
+	describe('the findSchemasWithLocation util', () => {
+		it('should take a list of SchemasWithLocation and return a function to search in this list', () => {
+			expect(findSchemaWithLocation).to.be.a('function');
+			expect(findSchemaWithLocation(schemasWithLocation)).to.be.a('function');
+		})
+
+		describe('the returned method', () => {
+			
+			let finder: (schema: SchemaI) => SchemaWithLocationI;
+
+			beforeEach(() => {
+				finder = findSchemaWithLocation(schemasWithLocation);
+			})
+			
+			it('should accept a schema and return the schema with location if one was found', () => {
+				expect(finder(schemas[0])).to.eql(schemasWithLocation[0]);
+				expect(finder(schemas[1])).to.eql(schemasWithLocation[1]);
+				expect(finder(schemas[2])).to.eql(schemasWithLocation[2]);
+			})
+
+			it('should return undefined if a schema could not be found', () => {
+				expect(finder({ name: 'a schema which does not exist'})).to.eql(undefined);
+			})
+
+			it('should return undefined if undefined or something else is passed', () => {
+				expect(finder(undefined)).to.eql(undefined);
+			})
+		})
+	})
+
+	describe('forEachReference util', () => {
+		it('should be a function', () => {
+			expect(forEachReference).to.be.a('function');
+		})
+
+		it('should call the callback for each reference', () => {
+			const callback = sinon.spy();
+			expect(callback.calledOnce).to.be.false;
+			forEachReference(schemas[3], callback);
+			expect(callback).to.be.calledThrice
+		})
+
+		describe('handles undefined values', () => {
+			it('should work if schema is undefined', () => {
+				expect(forEachReference(undefined, (schema: SchemaI) => null)).to.be.undefined;
+			})
+			
+			it('should work if callback is undefined', () => {
+				expect(forEachReference(schemas[3], undefined)).to.be.undefined;
+			})
+		})
+	})
+})
--- src/utils/schemaUtils.ts
+++ src/utils/schemaUtils.ts
@@ -3,8 +3,14 @@ import { SchemaWithLocationI, SchemaI } from '../interfaces/SchemaInterface';
 
 export const findSchemaWithLocation = (schemasWithLocation: SchemaWithLocationI[]) =>
 	(schema: SchemaI) =>
-		schemasWithLocation.filter(swl => swl.schema.name === schema.name)[0];
+		schema ? schemasWithLocation.filter(swl => swl.schema.name === schema.name)[0] : undefined;
 
 export const forEachReference = (schema: SchemaI, callback: (schema: SchemaI) => any) => {
-	Object.keys(schema.references).forEach(key => callback(schema.references[key]))
+	schema && schema.references ? (
+		Object.keys(schema.references).forEach(key => 
+			typeof callback === 'function' ? (
+				callback(schema.references[key])
+			) : (undefined)
+		)
+	) : ( undefined )
 }
