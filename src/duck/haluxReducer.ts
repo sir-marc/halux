be0bd1c603e8440c398fcad51b4b7a26dfbd06df
Simon Stucki <simon_stucki@hotmail.com>
2017-02-27 20:11:31 +0100

    multi-crawl functionality with state merging

--- src/duck/haluxReducer.ts
+++ src/duck/haluxReducer.ts
@@ -1,9 +1,50 @@
+import * as Immutable from 'immutable';
 import { HaluxSetStoreActionI } from "./haluxDuckInterfaces";
 import { SET_STORE } from "./haluxTypes";
 
-export const haluxReducer = ( state: any = {}, action: HaluxSetStoreActionI) => {
+// TODO: merge with convertToStore from hal-crawler
+const mergeState = (previous: any, newState: any) => {
+	let stateToReturn = previous;
+	newState.entrySeq().forEach((entry: any) => {
+		const key = entry[0];
+		const schemaEntrySet = entry[1];
+		const previousEntrySet = previous.get(key);
+		if(previousEntrySet === undefined) {
+			stateToReturn = stateToReturn.set(key, schemaEntrySet);
+		} else {
+			if(Immutable.Iterable.isIterable(previousEntrySet)) {
+				// TODO: might be, that some sub resources changed - this must be handled
+				//stateToReturn = stateToReturn.set(key, schemaEntrySet);
+				schemaEntrySet.forEach((resource :any) => {
+					const previousResource = previousEntrySet.find((previousResource :any) => resource.isModellingSameResourceAs(previousResource));
+					if(previousResource === undefined) {
+						stateToReturn = stateToReturn.updateIn([key], (schemas: any) => {
+	            return schemas.add(resource);
+	          });
+					} else {
+						if(resource.isNewerAs(previousResource)) {
+							stateToReturn = stateToReturn.updateIn([key], (schemas: any) => {
+								schemas.delete(previousResource);
+		            return schemas.add(resource);
+		          });
+						}
+					}
+				});
+			} else {
+				// must be a singular resource instance
+				if(schemaEntrySet.isNewerAs(previousEntrySet)) {
+					stateToReturn = stateToReturn.set(key, schemaEntrySet);
+				}
+			}
+		}
+	});
+	return stateToReturn;
+}
+
+export const haluxReducer = ( state: any = Immutable.Map<string, any>({}), action: HaluxSetStoreActionI) => {
 	switch (action.type) {
-		case SET_STORE: return action.payload;
+		case SET_STORE:
+			return mergeState(state, action.payload);
 		default: return state;
 	}
 };
--- src/middleware/halux.ts
+++ src/middleware/halux.ts
@@ -2,33 +2,48 @@ import * as Immutable from 'immutable';
 import get = require('lodash.get');
 import { HaluxActionI, HaluxActionObjectI } from '../interfaces/HaluxActionInterface';
 import { haluxSymbol } from './createHaluxAction';
-import { crawl, HalCrawlerConfigMap, Resource, Command, getResourceFromStore, action } from 'hal-crawler';
+import { crawl, HalCrawlerConfigMap, Resource, Command, getResourceFromStore, action, putInStoreAsPending } from 'hal-crawler';
 import { haluxActions } from '../duck';
-
 
 const scramble = (
 	config: HalCrawlerConfigMap,
-	done: (store: Immutable.Map<string, any>) => void,
 	command: Command,
 	actions: HaluxActionObjectI[],
-	store = Immutable.Map<string, any>(),
+	store: any,
+	location: string
 ): any => {
-	return crawl(config, command, store).then((store: any) => {
-		if(actions && actions[0]) {
-			const [ head, ...tail ] = actions;
-			const resource = getResourceFromStore(store, new Resource(head.schema, undefined, head.identifiers));
-			let promise: Promise<{}>;
-			if(resource === undefined) {
-				promise = crawl(config, new Command(resource, action.GET), store);
-			} else {
-				promise = Promise.resolve(store);
-			}
+	const state = Immutable.Map<string, any>(get(store.getState(), location));
 
-			promise.then((store:any) => {
-				scramble(config, done, new Command(resource, action.GET), tail, store);
-			});
-		} else {
-			done(store);
+	const demandedResource = command.getResource();
+	const resource = getResourceFromStore(state, new Resource(demandedResource.getSchema(), demandedResource.getLink(), demandedResource.getData()));
+	let promise: Promise<{}>;
+	let isFirstToCallForResource = true;
+	// first call to root object (empty state)
+	if(resource === undefined) {
+		const rootResource = new Resource(demandedResource.getSchema());
+		promise = crawl(config, new Command(rootResource, action.GET), state);
+		const newState = putInStoreAsPending(promise, rootResource, state);
+		store.dispatch(haluxActions.setStore(newState));
+	} else if(resource.isShallow() && !resource.isPending()) {
+		promise = crawl(config, new Command(resource, action.GET), state);
+		const newState = putInStoreAsPending(promise, resource, state);
+		store.dispatch(haluxActions.setStore(newState));
+	} else if(resource.isPending()) {
+		isFirstToCallForResource = false;
+		promise = resource.getPromise();
+	} else {
+		isFirstToCallForResource = false;
+		promise = Promise.resolve(state);
+	}
+
+	promise.then((state:any) => {
+		// another call has already posted the changes to the state - therefore we don't have to do it again
+		if(isFirstToCallForResource) {
+			store.dispatch(haluxActions.setStore(state));
+		}
+		if(actions.length !== 0) {
+			const [ head, ...tail ] = actions;
+			scramble(config, new Command(new Resource(head.schema, undefined, head.identifiers), action.GET), tail, store, location);
 		}
 	});
 };
@@ -39,11 +54,7 @@ export const createHalux = (halCrawlerConfig: HalCrawlerConfigMap, location = 'd
 			const actions = action.payload[haluxSymbol];
 			const [ head, ...tail ] = actions;
 
-			const done = (newStore: Immutable.Map<string, any>) => {
-				store.dispatch(haluxActions.setStore(newStore));
-			};
-
-			scramble(halCrawlerConfig, done, new Command(new Resource(head.schema)), tail, Immutable.Map<string, any>(get(store.getState(), location)));
+			scramble(halCrawlerConfig, new Command(new Resource(head.schema)), tail, store, location);
 
 		}
 		// middleware is not needed
